c2690b127a00fa88e3ac88011d332e092a9716d7
James <1146556298@qq.com>
2017-09-02 22:03:01 +0800

    ！

--- app/src/main/java/com/zbl/anju/db/DBManager.java
+++ app/src/main/java/com/zbl/anju/db/DBManager.java
@@ -15,10 +15,27 @@ public class DBManager {
 
 	private static DBManager mInstance;
 	private static final double ABS_LOC_3_KM = 0.035835;    //3公里对应的经纬度变化
+	private static final String QINIU_HOST = "http://ovi01lb03.bkt.clouddn.com";    //七牛云host
+
+	private List<String> xiaoquName = new ArrayList<>();
+	private List<String> description = new ArrayList<>();
+	private List<String> distance = new ArrayList<>();
+	private List<String> releaseTime = new ArrayList<>();
+	private List<String> price0 = new ArrayList<>();
+	private List<String> price1 = new ArrayList<>();
+	private List<String> price2 = new ArrayList<>();
+	private List<String> price3 = new ArrayList<>();
+	private List<String> hostName = new ArrayList<>();
+	private List<String> hostPhoneNum = new ArrayList<>();
+	private List<String> urlVideoThumbnail = new ArrayList<>();
+	private List<String> canDao = new ArrayList<>();
+	private List<String> canAddFir = new ArrayList<>();
+	private List<String> latLng = new ArrayList<>();
 
 	public DBManager() {
-
+		initFalseData();
 	}
+
 
 	public static DBManager getInstance() {
 		if (mInstance == null) {
@@ -71,8 +88,72 @@ public class DBManager {
 		List<House> houseList = new ArrayList<>();
 
 
-
-
 		return null;
+	}
+
+	/**
+	 * 初始化假数据
+	 */
+	private void initFalseData() {
+		/*小区名称*/
+		{
+			xiaoquName.add("华夏家园");
+			xiaoquName.add("文庭雅苑");
+			xiaoquName.add("美景天城");
+			xiaoquName.add("中景濠庭");
+			xiaoquName.add("枫蓝国际");
+			xiaoquName.add("新康家园");
+			xiaoquName.add("富泉花园");
+			xiaoquName.add("博雅西园");
+			xiaoquName.add("永泰园");
+			xiaoquName.add("翡翠城");
+			xiaoquName.add("世纪城·生活城");
+			xiaoquName.add("世纪东方城");
+			xiaoquName.add("纳帕溪谷");
+			xiaoquName.add("光彩国际公寓");
+		}
+		/*距离*/
+		{
+			distance.add("据我100m");
+			distance.add("据我300m");
+			distance.add("据我500m");
+			distance.add("据我800m");
+			distance.add("据我1km");
+			distance.add("据我1.3km");
+			distance.add("据我1.5km");
+			distance.add("据我1.7km");
+			distance.add("据我1.9km");
+			distance.add("据我2km");
+			distance.add("据我3km");
+			distance.add("据我5km");
+			distance.add("据我7.5km");
+			distance.add("据我8.3km");
+			distance.add("据我9.7km");
+			distance.add("据我11.2km");
+			distance.add("据我15.9km");
+		}
+		/*发布时间*/
+		{
+			releaseTime.add("09-03 8:35");
+			releaseTime.add("09-04 9:11");
+			releaseTime.add("09-05 10:25");
+			releaseTime.add("09-06 12:55");
+			releaseTime.add("09-07 13:39");
+			releaseTime.add("09-08 14:47");
+			releaseTime.add("09-09 15:20");
+			releaseTime.add("09-10 16:17");
+			releaseTime.add("09-11 17:26");
+			releaseTime.add("09-12 18:10");
+			releaseTime.add("09-13 19:52");
+			releaseTime.add("09-14 20:12");
+		}
+		/*价格*/
+		{
+			price0.add("700元/月");
+			price0.add("800元/月");
+			price0.add("800元/月");
+			price0.add("800元/月");
+			price0.add("800元/月");
+		}
 	}
 }
