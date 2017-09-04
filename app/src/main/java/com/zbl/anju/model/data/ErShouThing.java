f3502a15044961433be7733d26187dc85c10bd05
James <1146556298@qq.com>
2017-09-04 08:47:17 +0800

    二手物品

---
+++ app/src/main/java/com/zbl/anju/model/data/ErShouThing.java
@@ -0,0 +1,12 @@
+package com.zbl.anju.model.data;
+
+/**
+ * 二手物品
+ * Created by James on 17-9-4.
+ */
+
+public class ErShouThing {
+	private String name;
+	private String price;
+	private String distance;
+}
